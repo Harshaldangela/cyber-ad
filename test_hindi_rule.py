e3a12dec3d0b7e7ede47ba2dbd3d43d11882345c
Harshaldangela <1032221036@tcetmumbai.in>
2025-09-26 18:07:37 +0530

    final push

--- test_hindi_rule.py
+++
@@ -1,6 +0,0 @@
-from improved_rule_based_detector import ImprovedRuleBasedSpamDetector
-
-detector = ImprovedRuleBasedSpamDetector()
-prediction, confidence = detector.predict('बधाई हो! आपने ₹10,00,000 जीते हैं। राशि पाने के लिए लिंक पर क्लिक करें और अपना विवरण दर्ज करें')
-print('Prediction:', prediction)
-print('Confidence:', confidence)
